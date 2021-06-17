73c92ab3e723d9ba48bff8cc1213361805262b0e
Bhaiyahns45 <72096831+Bhaiyahns45@users.noreply.github.com>
2021-06-17 10:13:12 +0530

    Update check if given string is pangram.py

--- check if given string is pangram.py
+++ check if given string is pangram.py
@@ -1,3 +1,14 @@
+'''pangram is a string which conotain all the char from a-z .
+example-
+
+
+input1=Pack my box with five dozen liquor jugs.
+output1=Yes
+
+input2=A pangram or holoalphabetic sentence is a sentence
+output2=NO
+
+'''
 def checkPangram(s):
 
     s = s.replace(' ', '')
